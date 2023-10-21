acba71c1c2f2bb7e4c607a04cfce5f8e1c8fa821
mhiggins8450 <146852125+mhiggins8450@users.noreply.github.com>
2023-10-21 00:53:36 -0400

    Updated Classes Exercise

--- week3day2hw.js
+++ week3day2hw.js
@@ -1,17 +1,49 @@
-class Cat {
-    constructor(eyes, legs, fur, color, age) { //five properties
-        this.eyes = eyes;
-        this.legs = legs;
-        this.fur = fur;
-        this.age = age;
-        this.color = color; //five methods
-    }
-}
+// class Cat {
+//     name = 'snowball'
+//     age = 6
+//     color = 'white'
 
-const sphynx = new Cat(2, 4, false, 'translucent', 5) //instance #1
-const cheetah = new Cat(2, 4, true, 'spotted', 10) //instance #2
+//     play() {
+//         console.log(`${this.name} is playing.`)
+//     }
 
-console.log(sphynx)
-//Cat { eyes: 2, legs: 4, fur: false, age: 5, color: 'translucent' }
-console.log(cheetah)
-// Cat { eyes: 2, legs: 4, fur: true, age: 10, color: 'spotted' }
+//     purr() {
+//         console.log(`${this.name} is purring.`)
+//     }
+
+//     eat() {
+//         console.log(`${this.name} is eating.`)
+//     }
+// }
+
+// const cheetah = new Cat();
+// const lion = new Cat();
+
+// console.log(`cheetah`, cheetah);
+// console.log(`lion`, lion);
+
+// cheetah.play();
+// cheetah.purr();
+// cheetah.eat();
+// lion.play();
+// lion.purr();
+// lion.eat();
+
+
+// class Cat {
+//     constructor(eyes, legs, fur, color, age) { //five properties
+//         this.eyes = eyes;
+//         this.legs = legs;
+//         this.fur = fur;
+//         this.age = age;
+//         this.color = color; //five methods
+//     }
+// }
+
+// const sphynx = new Cat(2, 4, false, 'translucent', 5) //instance #1
+// const cheetah = new Cat(2, 4, true, 'spotted', 10) //instance #2
+
+// console.log(sphynx)
+// //Cat { eyes: 2, legs: 4, fur: false, age: 5, color: 'translucent' }
+// console.log(cheetah)
+// // Cat { eyes: 2, legs: 4, fur: true, age: 10, color: 'spotted' }
