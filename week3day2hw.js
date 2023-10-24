d93df8636728b513a240fe920043c0e4a2976917
mhiggins8450 <146852125+mhiggins8450@users.noreply.github.com>
2023-10-24 05:20:52 -0400

    Completed Inheritance/Factory Exercise

--- week3day2hw.js
+++ week3day2hw.js
@@ -31,32 +31,32 @@
 // lion.eat();
 
 // CREATED CLASS W/ CONSTRUCTOR
-// class Pirate {
-//     constructor(hook, pegleg, patch) { 
-//         this.hook = hook;
-//         this.pegleg = pegleg;
-//         this.patch = patch;
-//     }
-// }
+class Pirate {
+    constructor(hook, pegleg, patch) { 
+        this.hook = hook;
+        this.pegleg = pegleg;
+        this.patch = patch;
+    }
+}
 
-// const jollyRoger = [
-//     new Pirate(true, false, false),
-//     new Pirate(false, true, false),
-//     new Pirate(false, true, true),
-//   ];
+const jollyRoger = [
+    new Pirate(true, false, false),
+    new Pirate(false, true, false),
+    new Pirate(false, true, true),
+  ];
   
-//   const blackPearl = [
-//     new Pirate(false, true, true),
-//     new Pirate(true, false, true),
-//     new Pirate(true, false, true),
-//   ];
+  const blackPearl = [
+    new Pirate(false, true, true),
+    new Pirate(true, false, true),
+    new Pirate(true, false, true),
+  ];
   
-//   console.log("Jolly Roger Pirates:");
-//   jollyRoger.forEach((pirate, index) => {
-//     console.log(`Pirate ${index + 1}: Hook - ${pirate.hook}, Pegleg - ${pirate.pegleg}, Patch - ${pirate.patch}`);
-//   });
+  console.log("Jolly Roger Pirates:");
+  jollyRoger.forEach((pirate, index) => {
+    console.log(`Pirate ${index + 1}: Hook - ${pirate.hook}, Pegleg - ${pirate.pegleg}, Patch - ${pirate.patch}`);
+  });
   
-//   console.log("Black Pearl Pirates:");
-//   blackPearl.forEach((pirate, index) => {
-//     console.log(`Pirate ${index + 1}: Hook - ${pirate.hook}, Pegleg - ${pirate.pegleg}, Patch - ${pirate.patch}`);
-//   });
+  console.log("Black Pearl Pirates:");
+  blackPearl.forEach((pirate, index) => {
+    console.log(`Pirate ${index + 1}: Hook - ${pirate.hook}, Pegleg - ${pirate.pegleg}, Patch - ${pirate.patch}`);
+  });
